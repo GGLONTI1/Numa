5048596a31440c2dcebd799120d00341e1df3f1a
tskhvedian <t.tskhvediani@gmail.com>
2025-06-20 21:08:49 +0400

    fix: correct environment variable name for OpenAI API key and remove unnecessary error handling

--- lib/openai/config.ts
+++ lib/openai/config.ts
@@ -1,9 +1,5 @@
 import OpenAI from "openai";
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error("Cannot find key");
-}
-
 export const openai = new OpenAI({
   apiKey: process.env.OPENA_API_KEY,
 });
